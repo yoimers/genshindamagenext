305115d7ecbd19dfa8b09f0dceef6485b9c0d33f
yomei <hiseica2536@gmail.com>
2021-08-26 21:02:58 +0900

    aa

--- components/Board/BoardSidebar.tsx
+++ components/Board/BoardSidebar.tsx
@@ -25,10 +25,10 @@ export default function ContactSidebar({ data, loading, error, fetchMore }: Inpu
   }
 
   return (
-    <aside className="w-52 h-sidebarhight rounded-lg shadow-sm sticky top-2 p-1 text-textcolor">
+    <aside className="w-52 h-sidebarhight rounded-lg shadow-sm sticky top-2 p-1 text-textcolor overflow-auto">
       <ul>
         <Link href={`/boards/`}>
-          <a className="block pl-4 mx-1 mt-2 h-10 leading-10 rounded-lg bg-gray-700 shadow-xl focus:ring-0 ring-blue-100 ring-offset-2 ring-offset-bgc text-lg">
+          <a className="block pl-4 mx-1 mt-2 h-10 leading-10 rounded-lg bg-gray-900 shadow-xl focus:ring-0 ring-blue-100 ring-offset-2 ring-offset-bgc text-lg">
             掲示板へ戻る
           </a>
         </Link>
@@ -37,42 +37,42 @@ export default function ContactSidebar({ data, loading, error, fetchMore }: Inpu
           data.boards.boards.map((board: Board) => {
             return (
               <Link href={`/boards/${board.id}`} key={board.id}>
-                <a className="block pl-4 mx-1 mt-2 h-10 leading-10 rounded-lg bg-gray-700 shadow-xl focus:ring-0 ring-blue-100 ring-offset-2 ring-offset-bgc text-lg">
+                <a className="block pl-4 mx-1 mt-2 h-10 leading-10 rounded-lg bg-gray-700 shadow-xl focus:ring-0 ring-blue-100 ring-offset-2 ring-offset-bgc text-lg  overflow-hidden">
                   {board.title}
                 </a>
               </Link>
             );
           })}
       </ul>
-      <div>
-        {data.boards && data.boards.hasMore && (
-          <button
-            onClick={() => {
-              fetchMore({
-                variables: {
-                  after: data.boards.cursor as number,
-                },
-                updateQuery: (
-                  prev: any,
-                  { fetchMoreResult, ...rest }: { fetchMoreResult: any }
-                ) => {
-                  if (!fetchMoreResult) return prev;
-                  console.log(fetchMoreResult);
-                  return {
-                    ...fetchMoreResult,
-                    boards: {
-                      ...fetchMoreResult.boards,
-                      boards: [...prev.boards.boards, ...fetchMoreResult.boards.boards],
-                    },
-                  };
-                },
-              });
-            }}
-          >
-            Lead More
-          </button>
-        )}
-      </div>
+      {/* <div>
+  {data.boards && data.boards.hasMore && (
+    <button
+      onClick={() => {
+        fetchMore({
+          variables: {
+            after: data.boards.cursor as number,
+          },
+          updateQuery: (
+            prev: any,
+            { fetchMoreResult, ...rest }: { fetchMoreResult: any }
+          ) => {
+            if (!fetchMoreResult) return prev;
+            console.log(fetchMoreResult);
+            return {
+              ...fetchMoreResult,
+              boards: {
+                ...fetchMoreResult.boards,
+                boards: [...prev.boards.boards, ...fetchMoreResult.boards.boards],
+              },
+            };
+          },
+        });
+      }}
+    >
+      Lead More
+    </button>
+  )}
+</div> */}
     </aside>
   );
 }
