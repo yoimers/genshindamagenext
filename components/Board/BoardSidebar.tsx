4e8d9febe6bea2eb059b9e034c801d1b99590f9f
yomei <hiseica2536@gmail.com>
2021-08-28 04:36:01 +0900

    a

--- components/Board/BoardSidebar.tsx
+++ components/Board/BoardSidebar.tsx
@@ -45,35 +45,36 @@ export default function ContactSidebar({ data, loading, error, fetchMore }: Inpu
             );
           })}
       </ul>
-      {/* <div>
-  {data.boards && data.boards.hasMore && (
-    <button
-      onClick={() => {
-        fetchMore({
-          variables: {
-            after: data.boards.cursor as number,
-          },
-          updateQuery: (
-            prev: any,
-            { fetchMoreResult, ...rest }: { fetchMoreResult: any }
-          ) => {
-            if (!fetchMoreResult) return prev;
-            console.log(fetchMoreResult);
-            return {
-              ...fetchMoreResult,
-              boards: {
-                ...fetchMoreResult.boards,
-                boards: [...prev.boards.boards, ...fetchMoreResult.boards.boards],
-              },
-            };
-          },
-        });
-      }}
-    >
-      Lead More
-    </button>
-  )}
-</div> */}
+      <div>
+        {data.boards && data.boards.hasMore && (
+          <button
+            onClick={() => {
+              fetchMore({
+                variables: {
+                  after: data.boards.cursor as number,
+                },
+                updateQuery: (
+                  prev: any,
+                  { fetchMoreResult, ...rest }: { fetchMoreResult: any }
+                ) => {
+                  if (!fetchMoreResult) return prev;
+                  console.log(fetchMoreResult);
+                  return {
+                    ...fetchMoreResult,
+                    boards: {
+                      ...fetchMoreResult.boards,
+                      boards: [...prev.boards.boards, ...fetchMoreResult.boards.boards],
+                    },
+                  };
+                },
+              });
+            }}
+            className="block pl-4 mx-1 mt-2 h-10 leading-10 rounded-lg bg-gray-900 shadow-xl focus:ring-0 ring-blue-100 ring-offset-2 ring-offset-bgc text-lg"
+          >
+            Lead More
+          </button>
+        )}
+      </div>
     </aside>
   );
 }
